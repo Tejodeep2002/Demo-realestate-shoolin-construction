ad369618d3675692f7a486f73e3c30bc66d2723f
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2024-02-20 02:40:07 +0530

    build: setup all formater

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-
 export default function Home() {
-  return (
-    <main className="w-full h-auto">
-      
-    </main>
-  );
+	return (
+		<main className="h-auto w-full">
+			<section className="flex h-8 w-full justify-end gap-10 bg-black text-white">
+				<div>Call us: 8017194170</div>
+				<div>Call us: 8017194170</div>
+			</section>
+		</main>
+	);
 }
